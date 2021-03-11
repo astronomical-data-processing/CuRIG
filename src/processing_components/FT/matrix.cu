157c3c34af0e35ef066927dc03a97868a7f4d89a
CustomOriented <70754831+HLSUD@users.noreply.github.com>
2021-03-11 14:31:20 +0800

    Create matrix.cu

---
+++ src/processing_components/FT/matrix.cu
@@ -0,0 +1,13 @@
+/*
+Matrix related operations
+1. Boardcast
+2. Matrices element-wise operations
+3. Matrices Multiplication
+4. Matrix transpose
+
+//may need some specific operations for vertors
+*/
+
+#include "matrix.cuh"
+
+
