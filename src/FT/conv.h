d6247b55de3cb613607e5e24329e27b3e33883be
Honghao LIU <hliuco@cse.ust.hk>
2021-04-24 16:07:59 +0800

    debug for convtest

---
+++ src/FT/conv.h
@@ -0,0 +1,20 @@
+#ifndef __CONV_CUH__
+#define __CONV_CUH__
+
+//#include "../../include/curafft.h"
+
+#include <stdlib.h>
+#include <math.h>
+#include <cuda.h>
+#include <stdio.h>
+#include "../utils.h"
+#include "../../include/dataType.h"
+#include "conv_invoker.h"
+
+__global__ void conv_2d_nputsdriven(PCS *x, PCS *y, CUCPX *c, CUCPX *fw, int M, 
+	const int ns, int nf1, int nf2, PCS es_c, PCS es_beta, int pirange, INT_M* cell_loc);
+
+__global__ void conv_3d_nputsdriven(PCS *x, PCS *y, PCS *z, CUCPX *c, CUCPX *fw, int M,
+    const int ns, int nf1, int nf2, int nf3, PCS es_c, PCS es_beta, int pirange, INT_M* cell_loc)
+
+#endif
