7474858d4677b8f7320dcac4f13cbed8fe43a14c
Honghao LIU <hliuco@cse.ust.hk>
2021-04-24 21:59:41 +0800

    debug for convtest

--- include/curafft_opts.h
+++ include/curafft_opts.h
@@ -24,7 +24,7 @@ struct curafft_opts {
 	int gpu_conv_only; // 0: NUFFT, 1: spread or interpolation only
 	int gpu_method;
 
-	/*
+	
     curafft_opts(){
         gpu_device_id = 0;
         upsampfac = 2.0;
@@ -35,28 +35,7 @@ struct curafft_opts {
 		gpu_kerevalmeth = 0;
 		gpu_conv_only = 0;
     };
-	*/
-
-	/* following options are for gpu */
-    /*
-        int gpu_method;  // 1: nonuniform-pts driven, 2: shared mem (SM)
-	int gpu_sort;    // when NU-pts driven: 0: no sort (GM), 1: sort (GM-sort)
-
-
-	int gpu_binsizex; // used for 2D, 3D subproblem method
-	int gpu_binsizey;
-	int gpu_binsizez;
-
-	int gpu_obinsizex; // used for 3D spread block gather method
-	int gpu_obinsizey;
-	int gpu_obinsizez;
-
-	int gpu_maxsubprobsize;
-	int gpu_nstreams;
-	int gpu_kerevalmeth; // 0: direct exp(sqrt()), 1: Horner ppval default 0
-
-	int gpu_spreadinterponly; // 0: NUFFT, 1: spread or interpolation only
-    */
+	
 };
 
 #endif
---
+++ src/FT/test.cu
@@ -0,0 +1,6 @@
+#include "dataType.h"
+
+int test(){
+    //find the wrong header
+    return 1;
+}
