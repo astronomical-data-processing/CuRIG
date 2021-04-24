8c4e4a3955a0b517c79eb37bcfa978106ed92e0b
Honghao LIU <hliuco@cse.ust.hk>
2021-04-24 22:03:46 +0800

    debug for convtest

--- src/FT/test.cu
+++ src/FT/test.cu
@@ -1,9 +1,10 @@
 #include "dataType.h"
 #include "curafft_opts.h"
 #include "curafft_plan.h"
+#include "utils.h"
 
-int setup_conv_opts(conv_opts *c_opts, PCS eps, PCS upsampfac, int kerevalmeth){
+int setup_conv_opts(conv_opts *c_opts, PCS eps, PCS upsampfac, int kerevalmeth, curafft_plan *plan, curafft_opts opts){
     int ier = 0;
-    kerevalmeth = 1;
+    kerevalmeth += 1;
     return ier;
 }
