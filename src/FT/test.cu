19ec0d411a9d296c26bbcb7d1776f653536e3d92
Honghao LIU <hliuco@cse.ust.hk>
2021-04-24 22:02:35 +0800

    debug for convtest

--- src/FT/test.cu
+++ src/FT/test.cu
@@ -2,7 +2,8 @@
 #include "curafft_opts.h"
 #include "curafft_plan.h"
 
-int test(){
-    //find the wrong header
-    return 1;
+int setup_conv_opts(conv_opts *c_opts, PCS eps, PCS upsampfac, int kerevalmeth){
+    int ier = 0;
+    kerevalmeth = 1;
+    return ier;
 }
