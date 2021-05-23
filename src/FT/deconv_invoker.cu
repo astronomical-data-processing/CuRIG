3c17f34e8a4192778fad4bb18e2553c589e5400b
Honghao LIU <hliuco@cse.ust.hk>
2021-05-23 17:49:01 +0800

    change in deconv_invoker

--- src/FT/deconv_invoker.cu
+++ src/FT/deconv_invoker.cu
@@ -4,19 +4,42 @@ invoke deconv related kernels
 #include "deconv_invoker.h"
 
 int nupt_deconv(){
-
+    int ier = 0;
+    // niffty + cufinufft
+    
+    return ier;
 }
 
 int sorted_deconv(){
+    int ier = 0;
 
+    
+    return ier;
 }
 
-int curafft_deconv(curafft_plan *plan){
+int curafft_deconv(curafft_plan *plan, int cur_bth_size){
+    int ier = 0;
     int nf1 = plan->nf1;
     int nf2 = plan->nf2;
-    int num_w = plan->num_w;
+    int nf3 = plan->num_w;
+    int M = plan->M;
 
-    if(plan->opts.gpu_gridder_method == ){
-        nupt_deconv()
+    switch (plan->opts.gpu_gridder_method)
+    {
+        case 1:{
+            ier = nupt_deconv(nf1, nf2, nf3, M, plan, cur_bth_size);
+            break;
+        }
+        
+        case 2:{
+            ier = sorted_deconv(nf1, nf2, nf3, M, plan, cur_bth_size);
+            break;
+        }
+        default:{
+            cout<<"incorrect degridding method\n";
+            ier = 2;
+            break;
+        }
     }
+    return ier;
 }
