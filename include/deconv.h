f43c77595000d215664fd4cfbb154842eaae22f5
Honghao LIU <hliuco@cse.ust.hk>
2021-07-02 18:04:47 +0800

    gaussian quadrature cuda

--- include/deconv.h
+++ include/deconv.h
@@ -2,6 +2,6 @@
 #define __DECONV_H__
 
 #include "curafft_plan.h"
-
+void fourier_series_appro_invoker(PCS *fseries, PCS *k, conv_opts opts, int N)
 int curafft_deconv(curafft_plan *plan);
 #endif
--- src/FT/deconv.cu
+++ src/FT/deconv.cu
@@ -6,7 +6,69 @@ Deconvlution related kernels
 legendre_rule_fast cuda version should be implemented here
 */
 #include "deconv.h"
+#include "helper_cuda.h"
 #include "curafft_plan.h"
+#ifdef __cplusplus
+extern "C"
+{
+#include "legendre_rule_fast.h"
+}
+#else
+#include "legendre_rule_fast.h"
+#endif
+
+__global__ void fourier_series_appro(PCS *fseries, PCS *k, int N, PCS *g, PCS *x, int p){
+    int idx;
+    for(idx = blockDim.x * blockIdx.x + threadIdx.x; idx < N; idx+=gridDim.x*blockDim.x){
+        for(int i=0; i<p; i++){
+            fseries[idx] += g[i]*cos(x[i]*(k==NULL? idx: k[idx]));
+        }
+        fseries[idx] = 2*fseries[idx]; // add negative part
+    }
+}
+
+int fourier_series_appro_invoker(PCS *fseries, PCS *k, conv_opts opts, int N)
+{
+    /*
+        One dimensional Fourier series approximation. f(k) = int e^{ikx} f(x) dx.
+
+        Input: 
+            opts - convolution options
+            k - location of the series (on device)
+            N - number of k
+            flag - -1 or 1
+        Output: real(fk) // on device
+    */
+    // comments need to be revised
+    int ier = 0;
+    PCS alpha = opts.kw / 2.0; // J/2, half-width of ker z-support
+    // # quadr nodes in z (from 0 to J/2; reflections will be added)...
+    int p = (int)(2 + 3.0 * alpha); // not sure why so large? cannot exceed MAX_NQUAD
+    PCS g[MAX_NQUAD]; // intermediate result
+    double x[2 * MAX_NQUAD], w[2 * MAX_NQUAD];
+    legendre_compute_glr(2 * p, x, w); // only half the nodes used, eg on (0,1)
+    for (int n = 0; n < p; ++n) //using 2q points testing
+    {                                                              // set up nodes z_n and vals f_n
+        x[n] *= alpha;                                                // rescale nodes
+        g[n] = alpha * (PCS)w[n] * exp(opts.ES_beta * (sqrt(1.0 - opts.ES_c * x[n] * x[n])));  // vals & quadr wei
+        // a[n] = exp(2 * PI * IMA * (PCS)(nf / 2 - z[n]) / (PCS)nf); // phase winding rates
+    }
+    double *d_x;
+    PCS *d_g;
+    checkCudaErrors(cudaMalloc((void**)&d_x, sizeof(double)*p)); // change to constant memory
+    checkCudaErrors(cudaMalloc((void**)&d_g, sizeof(PCS)*p));
+
+    checkCudaErrors(cudaMemcpy(d_x, x, sizeof(double)*p, cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(d_g, g, sizeof(PCS)*p, cudaMemcpyHostToDevice));
+
+    int blocksize = 512;
+    fourier_series_appro<<<(N-1)/blocksize+1,blocksize>>>(fseries,k,N,d_g,d_x,p);
+    checkCudaErrors(cudaDeviceSynchronize());
+    checkCudaErrors(cudaFree(d_g));
+    checkCudaErrors(cudaFree(d_x));
+
+    return ier;
+}
 
 __global__ void deconv_1d(int N1, int nf1, CUCPX *fw, CUCPX *fk, PCS *fwkerhalf1, int flag){
     /*
