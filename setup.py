a8725ba60b29c3f1be24db378dea2ee1ad4e0562
HLSUD <stein.h.liu@gmail.com>
2021-08-02 15:12:26 +0800

    setup.py finished

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ import os
 import ctypes
 from setuptools import setup, Extension
 
+
+lib_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"lib/libcurafft.so")
 try:
-    lib = ctypes.cdll.LoadLibrary('libcufinufft.so')
+    lib = ctypes.cdll.LoadLibrary(lib_path)
 except Exception as e:
     print('CUDA shared libraries not found in library path.')
     raise(e)
+
 
 setup(
     name='curagridder',  
@@ -17,13 +20,14 @@ setup(
     description="GPU version of NUFFT and Radio astronomy gridder package",
     packages=['curagridder'],
     package_dir={'': 'python'},
-    url="https://github.com/HLSUD/NUFFT",
+    url="https://github.com/HLSUD/CURIG",
     install_requires=['numpy', 'pycuda', 'six'],
     python_requires='>=3.6',
+    zip_safe=False,
     ext_modules=[
-        Extension(name='NUFFT',
+        Extension(name='CURIG',
                   sources=[],
-                  libraries=['NUFFT'],
+                  libraries=['curafft'],
                   library_dirs=['lib'])
         ]
 )
